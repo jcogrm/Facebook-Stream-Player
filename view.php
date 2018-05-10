97b5a5d90bb0def51f67d74fabdfd1994b57b5f1
jcogrm <jcogrm@gmail.com>
2018-05-10 20:19:42 -0300

    JW Player version. HD option available.

--- view.php
+++ view.php
@@ -8,10 +8,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" media="screen" href="https://cdnjs.cloudflare.com/ajax/libs/video.js/5.11.9/video-js.min.css">
-        <link rel="stylesheet" media="screen" href="./template_view.css">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/videojs-resolution-switcher/0.4.2/videojs-resolution-switcher.min.css" />
 		<style>
             html,body{
             	height: 100%;
@@ -43,8 +40,5 @@
 		});
 	</script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="./script_view.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/video.js/5.11.9/ie8/videojs-ie8.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/video.js/5.11.9/video.min.js"></script>
     </body>
 </html>
